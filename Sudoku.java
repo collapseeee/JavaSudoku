a4443acf78650c6861c3b9286cf7f9d1ed5e21d0
COLLAPse <4174tt@gmail.com>
2024-10-02 00:53:36 +0700

    Number in each row aren't repeat.

--- Sudoku.java
+++ Sudoku.java
@@ -1,13 +1,6 @@
 public class Sudoku {
     public static void main(String[] args) {
-        int[][] sudokuTable = new int[9][9];
-        
-        for (int i=0; i<sudokuTable.length; i++) {
-            for (int j=0; j<sudokuTable.length; j++) {
-                sudokuTable[i][j] = (int)((Math.random()*9)+1);
-            }
-        }   
-        SudokuTablePrint(sudokuTable);
+        SudokuTablePrint(RandomUniqueNumber());
     }
     public static void SudokuTablePrint (int sudokuNumber[][]) {
         System.out.println("+---------+---------+---------+");
@@ -24,5 +17,24 @@ public class Sudoku {
                 System.out.println("+---------+---------+---------+");
             }
         }
+    }
+    public static int[][] RandomUniqueNumber () {
+        int[][] numberInArray = new int[9][9];
+        int remainingAvailableNumber = 0;
+        int randomIndexNumber = 0;
+        for (int i=0; i<numberInArray.length; i++) {
+            int[] availableNumbers = {1,2,3,4,5,6,7,8,9};
+            remainingAvailableNumber = availableNumbers.length;
+            for (int j=0; j<numberInArray.length; j++) {
+                //Random number for array index.
+                randomIndexNumber = (int)(Math.random()*remainingAvailableNumber);
+                //Assign the randomed number to the array.
+                numberInArray[i][j] = availableNumbers[randomIndexNumber];
+                //Reduce the range of number that can be random.
+                availableNumbers[randomIndexNumber] = availableNumbers[remainingAvailableNumber - 1];
+                remainingAvailableNumber--;
+            }
+        }
+        return numberInArray;
     }
 }
